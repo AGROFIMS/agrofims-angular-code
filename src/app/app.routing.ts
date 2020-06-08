0cc725984e279edba8d7d23e7f7d66c16664fdc3
ivanperezm <ivanpe834@gmail.com>
2020-06-08 17:19:20 -0500

    Added parameters to routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,77 @@
+import { Routes } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+
+import { AboutComponent } from './project/general-info/about/about.component';
+import { DocumentationComponent } from './project/general-info/documentation/documentation.component';
+import { ExperimentListComponent } from './project/experiment-core/experiment/experiment-list/experiment-list.component';
+import { ExperimentAddComponent } from './project/experiment-core/experiment/experiment-add/experiment-add.component';
+import { ExperimentManageComponent } from './project/experiment-core/experiment-manage/experiment-manage.component';
+import { ExpSiteEditComponent } from './project/experiment-core/exp-site/exp-site-edit/exp-site-edit.component';
+import { SiteListComponent } from './project/experiment-core/site/site-list/site-list.component';
+import { SiteAddComponent } from './project/experiment-core/site/site-add/site-add.component';
+import { SiteEditComponent } from './project/experiment-core/site/site-edit/site-edit.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      // {
+      //   path: '',
+      //   redirectTo: '/dashboard',
+      //   pathMatch: 'full'
+      // },
+      // {
+      //   path: '',
+      //   loadChildren:
+      //     () => import('./material-component/material.module').then(m => m.MaterialComponentsModule)
+      // },
+      // {
+      //   path: 'dashboard',
+      //   loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      // }
+      {
+        path: '',
+        redirectTo: '/about',
+        pathMatch: 'full'
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'documentation',
+        component: DocumentationComponent
+      },
+      {
+        path: 'experiments',
+        component: ExperimentListComponent
+      },
+      {
+        path: 'experiments/add',
+        component: ExperimentAddComponent
+      },
+      {
+        path: 'experiments/manage/:id',
+        component: ExperimentManageComponent
+      },
+      {
+        path: 'experiments/manage/:expId/site/:id',
+        component: ExpSiteEditComponent
+      },
+      {
+        path: 'sites',
+        component: SiteListComponent
+      },
+      {
+        path: 'sites/add',
+        component: SiteAddComponent
+      },
+      {
+        path: 'sites/edit/:id',
+        component: SiteEditComponent
+      }
+    ]
+  }
+];
