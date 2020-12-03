07ae77db213cad39d79041f8a78711dbaf9b4a45
ivanperezm <ivanpe834@gmail.com>
2020-12-03 16:32:42 -0500

    About module updated

--- src/app/project/general-info/about/about.component.ts
+++ src/app/project/general-info/about/about.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { AuthService } from '../../auth/services/auth.service';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
 
-  constructor() { }
+export class AboutComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    public dialog: MatDialog,
+  ) { }
 
   ngOnInit(): void {
   }
-
 }
