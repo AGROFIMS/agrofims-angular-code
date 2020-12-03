ae4f975aa2fe0f5c72ecfee51be133a5d2948a53
ivanperezm <ivanpe834@gmail.com>
2020-12-03 07:52:17 -0500

    Environment files updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://research.cip.cgiar.org/agrofims/api/app',
+  // baseUrl: 'https://research.cip.cgiar.org/agrofims/api/dev/0345/app',
+  baseUrl: 'https://research.cip.cgiar.org/agrofims/api/prod/0345/app',
+  baseUrlAuth: 'https://research.cip.cgiar.org/api/auth',
+  // baseUrlAuth: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
   baseUrl: 'https://research.cip.cgiar.org/agrofims/api/app',
+  baseUrlAuth: 'http://localhost:4000/api/auth',
+  // baseUrlUser: 'http://localhost:4000/api/user',
+  // baseUrlAuth: 'http://52.30.61.201:4000/api/auth',
+  // baseUrlUser: 'http://52.30.61.201:4000/api/user',
 };
